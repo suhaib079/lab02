7873392b42a2c28656c5bec7a3eef7467f1877d3
suhaib079 <suhaibabdennabi33@gmail.com>
2021-01-05 19:11:47 +0200

    lab3

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 'use strict'
 
 
+
+/////////////// 7 QUESTIONS ///////////////
+var score = 0;
 while (true) {
     var myLocation = prompt('do i live in amman ?');
 
     if (myLocation.toLowerCase() == "yes" || myLocation.toLowerCase() == "y") {
      // console.log('true thats where i live')
         alert('true thats where i live');
+        score++;
         break;
     }
     else if (myLocation.toLowerCase() == "no" || myLocation.toLowerCase() == "n") {
@@ -21,6 +25,7 @@ while (true) {
     if (major.toLowerCase() == "yes" || major.toLowerCase() == "y") {
        // console.log('yup')
         alert('yup');
+        score++;
         break;
     }
     else if (major.toLowerCase() == "no" || major.toLowerCase() == "n") {
@@ -35,6 +40,7 @@ while (true) {
     if (car.toLowerCase() == "yes" || car.toLowerCase() == "y") {
        // console.log('true')
         alert('true');
+        score++;
         break;
     }
     else if (car.toLowerCase() == "no" || car.toLowerCase() == "n") {
@@ -49,6 +55,7 @@ while (true) {
     if (bff.toLowerCase() == "yes" || bff.toLowerCase() == "y") {
         // console.log('yes')
         alert('true');
+        score++;
         break;
     }
     else if (bff.toLowerCase() == "no" || bff.toLowerCase() == "n") {
@@ -63,6 +70,7 @@ while (true) {
     if (job.toLowerCase() == "yes" || job.toLowerCase() == "y") {
        //console.log('true its my dream')
         alert('true its my dream');
+        score++;
         break;
     }
     else if (job.toLowerCase() == "no" || job.toLowerCase() == "n") {
@@ -82,3 +90,70 @@ while (getName === null || !isNaN(getName)){
     alert("Hello " + getName);
   }
 }
+
+
+/////////////// FIND MY FRUIT ///////////////
+var ct = 0;
+var flag = false;
+while (ct < 6){
+    // Init fruit array
+    var fruits = ["banana", "orange", "apple", "mango"];
+    // Ask user insert fruit
+    var find = prompt('think of my fav fruits');
+ 
+    // Check and find fruit if exist
+    for(var i = 0 ; i < fruits.length; i++){
+        if( fruits[i] == find ) {
+            alert('You are correct');
+            flag = true;
+            score++;
+            break;
+        }
+    }
+    // Quit loop if correct
+    if (flag == true) {
+        break;
+    }
+    // On fail print all fruits
+    if(ct == 5) {
+        alert(fruits)
+    }
+    else {
+        // If input wrong show message
+        alert("Please try again");
+    }
+
+    ct++;
+}
+
+
+
+
+
+
+
+
+
+
+
+/////////////// GUESS MY NUMBER ///////////////
+for (var i = 0 ;i <4 ; i++){
+    var guess = prompt('guess a numbe between 1 to 10');
+    var mynum = 6;
+    if(guess > mynum){
+        alert('to high')
+    }
+    if(guess < mynum){
+        alert('to low')
+    }
+    if(guess == mynum){
+        alert('correct');
+        score++;
+        break;
+    }
+    if(i == 3){
+        alert('the correct answer is ' + mynum);
+    }
+}
+alert('ur score is ' + score + ' out of 7 ');
+
